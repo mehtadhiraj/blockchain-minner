791f8f02d49e6e4a1241eca384f4a4c7581bbfc3
mehtadhiraj <mehtadhiraj21@gmail.com>
2020-02-28 14:00:43 +0530

    Minner logs added

--- firebase.js
+++ firebase.js
@@ -11,13 +11,16 @@ let db = admin.database();
 let ref = db.ref("/transactions");
 
 ref.on("child_added", function(snapshot) {
+  console.info("New Transaction Added.");
   let data = snapshot.key;   //Data is in JSON format.
   let keyRef = db.ref('/transactions/'+data);
   keyRef.once('value', function(snap){
     // console.log(snap.val());
     let hash = snap.val().hash;
+    console.info("Calculating Nonce...");
     let nonce = proofOfWork(hash);
     // console.log(nonce);
     keyRef.child('nonce').push(nonce);
+    console.info("Nonce Added.");
   });
 });
