a6e9f1ad087fc8c825c9e2b6a18949cb0a301c0c
mehtadhiraj <mehtadhiraj21@gmail.com>
2020-03-29 05:48:40 +0530

    Blockhain ins distributed

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
-let admin = require("firebase-admin");
-let { proofOfWork } = require('./minnnigAlgo');
-let serviceAccount = require("./blockchain-banking.json");
+const admin = require("firebase-admin");
+const { proofOfWork } = require('./minnnigAlgo');
+const serviceAccount = require("./blockchain-banking.json");
+let chainData = {};
 require('dotenv').config();
 
 admin.initializeApp({
@@ -10,18 +11,50 @@ admin.initializeApp({
 
 let db = admin.database();
 let ref = db.ref("/transactions");
+let broadCastRef = db.ref("/broadcast");
 
 ref.on("child_added", function(snapshot) {
+  console.log(chainData);
+  
   console.info("New Transaction Added.");
   let data = snapshot.key;   //Data is in JSON format.
   let keyRef = db.ref('/transactions/'+data);
-  keyRef.once('value', function(snap){
-    // console.log(snap.val());
-    let hash = snap.val().hash;
-    console.info("Calculating Nonce...");
-    let nonce = proofOfWork(hash);
-    // console.log(nonce);
-    keyRef.child('nonce').push(nonce);
-    console.info("Nonce Added.");
-  });
+  console.log({ data: data.split('-'), len: data.split('-').length });
+  
+  if(data.split('-').length == 3){
+    keyRef.once('value', function(snap){
+      // console.log(snap.val());
+      let hash = snap.val().hash;
+      console.info("Calculating Nonce...");
+      let nonce = proofOfWork(hash);
+      // console.log(nonce);
+      keyRef.child('nonce').push(nonce);
+      console.info("Nonce Added.");
+    });
+  }else{
+    keyRef.once('value', function(snap){
+      let encryptedChain = snap.val().encryptedChain;
+      let hash = chainData[data.split('-')[0]];
+      if(hash){
+        keyRef.child('hash').push(hash);
+      }else{
+        chainData[data.split('-')[0]] = encryptedChain;
+        keyRef.child('hash').push(encryptedChain);
+      }
+      console.log({ encryptedChain, hash, chainData });
+      console.info("Chain hash added.");
+    })
+  }
 });
+
+broadCastRef.on("child_added", function(snapshot){
+  let broadData = snapshot.key;
+  let broadKeyRef = db.ref('broadcast/'+broadData);
+  broadKeyRef.once('value', function(snap){
+    let chain = snap.val().chain;
+    chainData[broadData.split('-')[0]] = chain;
+  })
+  console.log(chainData);
+})
+
+console.log("Minner Started.");
